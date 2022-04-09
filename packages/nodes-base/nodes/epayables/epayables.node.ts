360dd84f2a35c6f5fdf01bd7b3133bfbe0abde96
Michael Hanna <michael_hanna@condenast.com>
2022-04-09 01:02:10 -0400

    implment ePayables Node

--- packages/nodes-base/nodes/epayables/epayables.node.ts
+++ packages/nodes-base/nodes/epayables/epayables.node.ts
@@ -9,17 +9,88 @@ import {
 } from 'n8n-workflow';
 
 
+import {
+	IExecuteFunctions,
+} from 'n8n-core';
 
-export class ePayables implements INodeType {
+
+
+export class Epayables implements INodeType {
 	description: INodeTypeDescription = {
-        
-    }
+		displayName: 'ePayables Connector',
+		name: 'ePayables',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume ePayables API',
+		defaults: {
+
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name: 'Contact',
+						value: 'contact',
+					},
+				],
+				default: 'contact',
+				required: true,
+				description: 'Resource to consume',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: [
+							'contact',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create a contact',
+					},
+				],
+				default: 'create',
+				description: 'The operation to perform.',
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: [
+							'create',
+						],
+						resource: [
+							'contact',
+						],
+					},
+				},
+				default:'',
+				description:'Primary email for the contact',
+			},
+		],
+};
 
     async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
-        const returnData: IDataObject[] = [];
-		
-        return returnData;
+		let resource = this.getNodeParameter("resource",0) as string;
+
+		return [this.helpers.returnJsonArray( { json: { "r": resource } })];
     }
 
 
